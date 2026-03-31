c259e15e0c52f0e8da7e718f29e96f2db7de2f15
Hebe-lab-2025 <90542908+Hebe-lab-2025@users.noreply.github.com>
2026-03-31 02:14:22 -0700

    update

---
+++ cdk-image-upload/small-cdk-s3/infra/lib/infra-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket
+    const bucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // Output bucket name
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: bucket.bucketName,
+    });
+  }
+}
